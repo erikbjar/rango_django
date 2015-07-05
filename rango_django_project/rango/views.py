d1723eee52c637048b2fa26acd27bcdd93ca53a5
erikbjar <erik@mie.utoronto.ca>
2015-07-05 11:00:05 +0100

    view setup

--- rango_django_project/rango/views.py
+++ rango_django_project/rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says hey there world!, here is the <br/> <a href='/rango/about' about </a> page")
+
+def about(request):
+    return HttpResponse("Rango says, go back to the <br/> <a href='/rango/'> index </a> page")
